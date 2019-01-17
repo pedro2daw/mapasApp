2ccc3ecb946ce4bc08c109400369309eebdc1656
Tony <malg61@gmail.com>
2019-01-17 13:25:07 +0100

    Añadido srcs

--- application/views/header.php
+++ application/views/header.php
@@ -3,10 +3,14 @@
 <head>
     <meta charset="utf-8" />
         <title>Superposición de Mapas</title>
-        <!-- <link rel="stylesheet" type="text/css" href="< ##################  ?php echo base_url()?>/assets/style/estilo.css" /> -->
-        <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- <link rel="stylesheet" type="text/css" href="< ##################  ?php echo base_url()?>/assets/style/estilo.css" /> -->
+        <!-- Required meta tags -->
+        <!-- AJAX -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!-- SCRIPT JS -->
+        <script src=<?php echo base_url("assets/js/Mapas.js"); ?>></script>
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
