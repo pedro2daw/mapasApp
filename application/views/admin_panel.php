d3c9e5d31893cf729f75813fe111a9ee473f5bc9
Pedro Lopez <pedro2daw.celia@gmail.com>
2019-01-20 19:57:40 +0100

    testeo insercion calles

--- application/views/admin_panel.php
+++ application/views/admin_panel.php
@@ -6,6 +6,7 @@ echo '
             <nav class="nav nav-pills flex-column flex-sm-row">
                 <a class="flex-sm-fill text-sm-center nav-link active" href="#">Mapas</a>
                 <a class="flex-sm-fill text-sm-center nav-link" href="#">Laminas</a>
+                <a class="flex-sm-fill text-sm-center nav-link" href="">Calles</a>
                 <a class="flex-sm-fill text-sm-center nav-link" href="#">Configuración</a>
             </nav>
         </div>
@@ -152,6 +153,76 @@ echo '
 
         <!-- ****************** CUERPO DEL CUADRO MODAL *********************** --> 
         <!-- ****************** INSERCIÓN DE UN MAPA    *********************** -->";
+
+        echo '
+        <!-- *********************** INSERCIÓN DE UNA CALLE ************************** -->
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#streetModal">
+        INSERTAR CALLE
+        </button>
+    
+        <div class="modal fade" id="streetModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            <!-- ****************** CUERPO DEL CUADRO MODAL STREET *********************** --> ';
+    
+            echo form_open('');
+            echo "<div class='form-group'>
+                    <label for='nombre'>Nombre de la calle</label>
+                    <input type='text' class='form-control' placeholder='Introduce el nombre de la calle' name='nombre' id='nombre' required/> 
+                </div>
+                
+
+                <div class='form-group'>
+                <label for='slide_list'>Selecciona la lámina</label>
+                <select>
+                    <option value='1'> ALMERIA </option>
+                    <option value='1'> ALMERIA 1945 </option>
+                </select>
+            
+                ";
+          echo "</div>";
+                
+    
+    
+                
+    
+    
+            
+        echo "
+            </div>
+                <div class='modal-footer'>
+                        <button type='button' class='btn btn-secondary' data-dismiss='modal'>Cerrar</button>
+                        <input type='submit' class='btn btn-primary' value='Insertar calle'/>
+                    </form> 
+                </div>
+            </div>
+        </div>
+        </div>
+    
+            <!-- ****************** CUERPO DEL CUADRO MODAL *********************** --> 
+            <!-- ****************** INSERCIÓN DE UN MAPA    *********************** -->";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     echo '</div>';
     echo '</div>';
---
+++ application/views/test_insert_street.php
@@ -0,0 +1,55 @@
+<?php 
+echo '
+<div class="container-fluid">
+
+    <div class="row">
+        <div class="col-md-12">';
+
+    echo '
+    <!-- *********************** INSERCIÓN DE UNA CALLE ************************** -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#streetModal">
+    INSERTAR CALLE
+    </button>
+
+    <div class="modal fade" id="streetModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+        <!-- ****************** CUERPO DEL CUADRO MODAL STREET *********************** --> ';
+
+        echo form_open('');
+        echo "<div class='form-group'>
+                <label for='nombre'>Nombre de la calle</label>
+                <input type='text' class='form-control' placeholder='Introduce el nombre de la calle' name='nombre' id='nombre' required/> 
+            </div>
+            <div class='form-group'>
+                <label for='slide_list'>Selecciona la lámina</label>
+                <select multiple>
+                    <option value='1'> ALMERIA </option>
+                    <option value='1'> ALMERIA 1945 </option>
+                </select>
+            </div>";
+    echo "
+        </div>
+            <div class='modal-footer'>
+                    <button type='button' class='btn btn-secondary' data-dismiss='modal'>Cerrar</button>
+                    <input type='submit' class='btn btn-primary' value='Insertar calle'/>
+                </form> 
+            </div>
+        </div>
+    </div>
+    </div>
+
+        <!-- ****************** CUERPO DEL CUADRO MODAL *********************** --> 
+        <!-- ****************** INSERCIÓN DE UN MAPA    *********************** -->";
+
+    echo '</div>';
+    echo '</div>';
+echo '</div>';
+
