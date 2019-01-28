14debf44a8855672689721cad0ffc5629921d89f
Tony <malg61@gmail.com>
2019-01-28 12:22:56 +0100

    PUsh pa el pedro

--- assets/mapasApp.sql
+++ assets/mapasApp.sql
@@ -6,10 +6,10 @@ use mapas;
 
 DROP TABLE IF EXISTS usuarios;
 DROP TABLE IF EXISTS mapas;
-DROP TABLE IF EXISTS laminas;
+-- DROP TABLE IF EXISTS laminas;
 DROP TABLE IF EXISTS hotspots;
 DROP TABLE IF EXISTS calles;
-DROP TABLE IF EXISTS calles_laminas;
+DROP TABLE IF EXISTS mapas_calles;
 
 CREATE TABLE IF NOT EXISTS `ci_sessions` (
         `id` varchar(128) NOT NULL,
@@ -35,16 +35,19 @@ CREATE TABLE mapas(
     nivel SMALLINT NOT NULL,
     ancho INT NOT NULL,
     altura INT NOT NULL,
+    fecha_de_subida DATETIME NOT NULL,
 
         id_paquete INT UNSIGNED NOT NULL
 );
 
 
-CREATE TABLE paquete_mapas(
+CREATE TABLE paquetes(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    fecha_subida DATETIME NOT NULL,
     descripcion VARCHAR(250) NOT NULL
 );
 
+/*
 CREATE TABLE laminas(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     imagen VARCHAR(250) NOT NULL,
@@ -58,6 +61,7 @@ CREATE TABLE laminas(
     punto3_left SMALLINT ,    
     id_mapa INT UNSIGNED
 );
+*/
 
 CREATE TABLE calles(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -67,7 +71,7 @@ CREATE TABLE calles(
     ano_fin SMALLINT NOT NULL
 );
 
-CREATE TABLE laminas_calles(
+CREATE TABLE mapas_calles(
     id_map INT UNSIGNED UNIQUE,
     id_lamina INT UNSIGNED UNIQUE,
     id_calle INT UNSIGNED UNIQUE
@@ -79,14 +83,22 @@ CREATE TABLE puntos(
     punto_x INT UNSIGNED NOT NULL,
     punto_y INT UNSIGNED NOT NULL
 );
+
 CREATE TABLE hotspots (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     imagen VARCHAR(250) NOT NULL,
     descripcion VARCHAR(600) NOT NULL,
     punto_x INT UNSIGNED NOT NULL,
     punto_y INT UNSIGNED NOT NULL,
-    id_lamina INT UNSIGNED
+    id_mapa INT UNSIGNED
 );
 
 INSERT INTO usuarios VALUES (null, 'admin','1');
--- INSERT INTO mapas VALUES (null,'Almería del Siglo XIX', 'Mapa de Almería del Siglo XIX Perez de Rozas','Almeria',1890,'/assets/img/mapas/1_almeria.jpg/',1,1000,1000);
+INSERT INTO mapas VALUES (null,'Almería del Siglo XIX', 'Mapa de Almería del Siglo XIX Perez de Rozas','Almeria',1890,'/assets/img/mapas/7_c_medium.jpg',1,1000,1000,10,10);
+INSERT INTO mapas VALUES (null,'Almería del Siglo XXI', 'Mapa de Almería del Siglo XXI','Almeria',2017,'/assets/img/mapas/7_o_medium.jpg',2,1000,1000,10,10);
+
+INSERT INTO paquetes VALUES (null,'1000-01-01 00:00:00', 'Descripción');
+
+INSERT INTO paquetes VALUES (null,'1000-02-01 00:00:00', 'Descripción');
+
+INSERT INTO paquetes VALUES (null,'1000-03-01 00:00:00', 'Descripción');
