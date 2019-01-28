1b96b9215d1084ba38a53cf899e209b03fc5a75c
Tony <malg61@gmail.com>
2019-01-28 13:39:56 +0100

    añado create_session()

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -24,6 +24,7 @@ class Login extends Security{
             $this->load->view('template',$data);
         }
         else{
+            create_session();
             $data['ListaMapas'] = $this->modelMapas->get_all();
             $data['ListaPaquetes'] = $this->modelPaquetes->get_all();
             // var_dump($data['ListaPaquetes']);
