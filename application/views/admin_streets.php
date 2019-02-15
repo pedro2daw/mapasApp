42249e31fc05c026225cf94b6d9ad08ca98bb09b
Tony <malg61@gmail.com>
2019-02-15 13:18:04 +0100

    streets fixed

--- application/views/admin_streets.php
+++ application/views/admin_streets.php
@@ -1,9 +1,6 @@
-<?php 
-echo '
 <div class="container-fluid">
 
-    <br><br><br><br><br>';
-    echo'<div class="table-responsive">
+    <div class="table-responsive">
             <table class="table table-hover">
                 <thead>
                     <tr>
@@ -17,7 +14,9 @@ echo '
                         <th></th>
                     </tr>
                 </thead>
-                <tbody>';
+                <tbody>
+
+                <?php 
                 for($i = 0; $i < count($listaCalles);$i++){
                     $each_street = $listaCalles[$i];
                     echo ("<tr><td>".$each_street["id"]."</td>");
@@ -27,104 +26,95 @@ echo '
                     echo ("<td>".$each_street["ano_fin"]."</td>");
                     echo ("<td>".$each_street["id_mapa"]."</td>");
                     echo("<td>");
-                            echo anchor("Streets/update_street/".$each_street['id'],"<span class='far fa-edit'></span>","class='btn btn-info'");
-                           echo ' <a href="#" title="Modificación de coordenadas" data-toggle="popover" data-trigger="focus" data-content="Si desea actualizar las coordenadas de la calle, elimine la calle e insertela de nuevo"><span class="far fa-question-circle"></span></a> ';
+                    echo anchor("Streets/update_street/".$each_street['id'],"<span class='far fa-edit'></span>","class='btn btn-info'");
+                    echo '<a href="#" title="Modificación de coordenadas" data-toggle="popover" data-trigger="focus" data-content="Si desea actualizar las coordenadas de la calle, elimine la calle e insertela de nuevo"><span class="far fa-question-circle"></span></a> ';
                     echo("</td>");  
                     echo("<td>");
-                            echo anchor("Streets/delete_street/".$each_street['id'],"<span class='fas fa-trash-alt'></span>","class='btn btn-danger'");
+                    echo anchor("Streets/delete_street/".$each_street['id'],"<span class='fas fa-trash-alt'></span>","class='btn btn-danger'");
                     echo("</td></tr>");
                 }
-echo '                    
+                ?>      
+
                 </tbody>
             </table>
         </div>
 
     <div class="row">
-        <div class="col-md-12">';
-
-    echo '
+        <div class="col-md-12">
     <!-- *********************** INSERCIÓN DE UNA CALLE ************************** -->
-    
-    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#streetModal">
+    <button type="button" class="btn btn-info" data-toggle="modal" data-target="#streetModal">
     <i class="fas fa-plus-circle" style="font-size:45px;"></i>
     </button>
 
     <div class="modal fade bd-example-modal-xl" id="streetModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-xl" role="document">
         <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalCenterTitle">Insertar calle</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body">
-        <!-- ****************** CUERPO DEL CUADRO MODAL STREET *********************** --> ';
+            <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalCenterTitle">Insertar calle</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                <!-- ****************** CUERPO DEL CUADRO MODAL STREET *********************** --> 
+                <?php echo form_open('Streets/insert_coords');?>
+                <div class='form-group'>
+                    <label for='nombre'>Nombre de la calle</label>
+                    <input type='text' class='form-control' placeholder='Introduce el nombre de la calle' name='nombre' id='nombre' required/> 
+                </div>
 
-        echo form_open('Streets/insert_coords');
-        echo "<div class='form-group'>
-                <label for='nombre'>Nombre de la calle</label>
-                <input type='text' class='form-control' placeholder='Introduce el nombre de la calle' name='nombre' id='nombre' required/> 
-            </div>";
+                <div class='form-group'>
+                    <label for='tipo'>Tipo de via</label>
+                        <select id='tipo' name='tipo' class='form-control'>
+                                <option value='Avenida'>Avenida</option>
+                                <option value='Calle'>Calle</option>
+                                <option value='Callejon'>Callejón</option>
+                                <option value='Camino'>Camino</option>
+                                <option value='Carretera'>Carretera</option>
+                                <option value='Glorieta'>Glorieta</option>
+                                <option value='Pasaje'>Pasaje</option>
+                                <option value='Paseo'>Paseo</option>
+                                <option value='Plaza'>Plaza</option>
+                                <option value='Poligono'>Poligono</option>
+                                <option value='Rambla'>Rambla</option>
+                                <option value='Residencia'>Residencia</option>
+                                <option value='Ronda'>Ronda</option>
+                                <option value='Travesia'>Travesía</option>
+                                <option value='Urbanizacion'>Urbanización</option>
+                                <option value='Via'>Via</option>
+                        </select>
+                </div>
 
-        echo "<div class='form-group'>
-                <label for='tipo'>Tipo de via</label>
-                
-                <select id='tipo' name='tipo' class='form-control'>
-                        <option value='Avenida'>Avenida</option>
-                        <option value='Calle'>Calle</option>
-                        <option value='Callejon'>Callejón</option>
-                        <option value='Camino'>Camino</option>
-                        <option value='Carretera'>Carretera</option>
-                        <option value='Glorieta'>Glorieta</option>
-                        <option value='Pasaje'>Pasaje</option>
-                        <option value='Paseo'>Paseo</option>
-                        <option value='Plaza'>Plaza</option>
-                        <option value='Poligono'>Poligono</option>
-                        <option value='Rambla'>Rambla</option>
-                        <option value='Residencia'>Residencia</option>
-                        <option value='Ronda'>Ronda</option>
-                        <option value='Travesia'>Travesía</option>
-                        <option value='Urbanizacion'>Urbanización</option>
-                        <option value='Via'>Via</option>
-                </select>
-            </div>";
+                <div class='form-group'>
+                    <label for='aInicio'>Año de inicio</label>
+                    <input type='number' min='1' class='form-control' placeholder='Introduce el año de inicio de la calle' name='aInicio' id='aInicio'/> 
+                </div>
 
-        echo "<div class='form-group'>
-                <label for='aInicio'>Año de inicio</label>
-                <input type='number' min='1' class='form-control' placeholder='Introduce el año de inicio de la calle' name='aInicio' id='aInicio'/> 
-            </div>";
+                <div class='form-group'>
+                    <label for='aFin'>Año final</label>
+                    <input type='number' min='1' class='form-control' placeholder='Introduce el año final de la calle' name='aFinal' id='aFinal'/> 
+                </div>
 
-        echo "<div class='form-group'>
-            <label for='aFin'>Año final</label>
-            <input type='number' min='1' class='form-control' placeholder='Introduce el año final de la calle' name='aFinal' id='aFinal'/> 
-        </div>";
+                <div class='form-group'>
+                    <label for='slide_list'>Selecciona la lámina</label>
+                    <?php echo form_dropdown('mapa', $mapas_disponibles,'','class="form-control form-control-md"'); ?>
+                        <!-- añadir mas adelante que se seleccione un mapa, y en funcion de ese mapa solo salgan las laminas de ese mapa -->
+                </div>
+                </div> <!-- modal body -->
 
-
-        echo"
-            <div class='form-group'>
-                <label for='slide_list'>Selecciona la lámina</label>";
-                
-                 echo form_dropdown('mapa', $mapas_disponibles,'','class="form-control form-control-md"') ;
-                // añadir mas adelante que se seleccione un mapa, y en funcion de ese mapa solo salgan las laminas de ese mapa
-        echo"
-            </div>";
-
-    echo "
-        </div>
-            <div class='modal-footer'>
-                    <input type='reset' class='btn btn-secondary' value='Reestablecer formulario' />
+                <div class='modal-footer'>
+                    <input type='reset' class='btn btn-secondary' value='Reestablecer formulario'/>
                     <input type='submit' class='btn btn-primary' value='Siguiente'/>
-                </form> 
+                    <?php echo form_close();?>
+                </div>
             </div>
         </div>
     </div>
-    </div>
 
         <!-- ****************** CUERPO DEL CUADRO MODAL *********************** --> 
-        <!-- ****************** INSERCIÓN DE UNA CALLE    *********************** -->";
+        <!-- ****************** INSERCIÓN DE UNA CALLE    *********************** -->
 
-        echo '</div>';
-    echo '</div>';
-echo '</div>';
+        </div>
+    </div>
+</div>
 
