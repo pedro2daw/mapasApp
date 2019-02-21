78f3e9e08e3f81fb3c82379d97f7c2e510f1152f
[edi2012] <[voicueduardadrian@yahoo.es]>
2019-02-21 14:35:04 +0100

    A

--- application/views/admin_hotspots.php
+++ application/views/admin_hotspots.php
@@ -61,7 +61,6 @@
 </script>
 
 <div class="container-fluid">
-
     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
