764e98b1468b1ac3bf9cb91b43cbe010aafdf66b
Tony <malg61@gmail.com>
2019-04-12 09:55:45 +0200

    Footer y Login

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,10 +1,19 @@
-<br><br>
-<span id="footer" class="text-center">&copy; Mapas Developers </span>
+
+
+</div> <!-- cierro container fluid -->
+<div id='footer'>
+    <span class="text-center">&copy; Antonio Jiménez FCT Intermark </span>
+
+    
+    <ul>
+        </li><a id='enlacegit' href="https://github.com/ajl200"><span class="fab fa-github"></span> Antonio Jiménez </a></li>
+    </ul>
+</div>
 
 <!-- Optional JavaScript -->
     <!-- Popper.js, then Bootstrap JS -->
-    <script src="<?php echo base_url()?>assets/js/bootstrap/popper.min.js" crossorigin="anonymous"></script>
-    <script src="<?php echo base_url()?>assets/js/bootstrap/bootstrap.min.js" crossorigin="anonymous"></script>
-    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 </body>
 </html>
---
+++ application/views/footer1.php
@@ -0,0 +1,10 @@
+<br><br>
+<span id="footer" class="text-center">&copy; Mapas Developers </span>
+
+<!-- Optional JavaScript -->
+    <!-- Popper.js, then Bootstrap JS -->
+    <script src="<?php echo base_url()?>assets/js/bootstrap/popper.min.js" crossorigin="anonymous"></script>
+    <script src="<?php echo base_url()?>assets/js/bootstrap/bootstrap.min.js" crossorigin="anonymous"></script>
+    
+</body>
+</html>
