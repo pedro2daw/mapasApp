aba278336cd66576ae56ac584edceea5d67c6a35
[edi2012] <[voicueduardadrian@yahoo.es]>
2019-04-26 10:10:58 +0200

    Problemas front resueltos.

--- application/views/index.php
+++ application/views/index.php
@@ -25,6 +25,10 @@
     <link href="<?php echo base_url()?>/assets/css/freelancer.min.css" rel="stylesheet">
 
     <style>
+        .masthead {
+            padding: 100px !important;
+        }
+        
         .navbar {
             background-color: #d33529 !important;
         }
@@ -304,3 +308,22 @@
             </div>
         </div>
     </footer>
+    <br><br>
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url()?>/assets/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url()?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="<?php echo base_url()?>/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?php echo base_url()?>/assets/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="<?php echo base_url()?>/assets/js/jqBootstrapValidation.js"></script>
+    <script src="<?php echo base_url()?>/assets/js/contact_me.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="<?php echo base_url()?>/assets/js/freelancer.min.js"></script>
+
+</body>
+
+</html>
