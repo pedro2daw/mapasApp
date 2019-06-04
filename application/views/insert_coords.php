ac6cafa92963a177694ee95027d77e6a9d375eda
Pedro Lopez <pedro2daw.celia@gmail.com>
2019-06-04 09:27:39 +0200

    elimino botones de prueba

--- application/views/insert_coords.php
+++ application/views/insert_coords.php
@@ -253,6 +253,67 @@ insertar_con_punto = false;
     
     // TEST PARA CAPTURA DE PANTALLA //
 
+    // TEST PARA EL ZOOM DONDE EL RATON //
+    /*
+    $(document).ready(function (){
+        new ScrollZoom($('#prueba'),3,0.1)
+    });
+
+    function ScrollZoom(container,max_scale,factor){
+        var target = container.children().first()
+        var size = {w:target.width(),h:target.height()}
+        var pos = {x:0,y:0}
+        var zoom_target = {x:0,y:0}
+        var zoom_point = {x:0,y:0}
+        var scale = 1
+        target.css('transform-origin','0 0')
+        target.on("mousewheel DOMMouseScroll",scrolled)
+    
+        function scrolled(e){
+            var offset = container.offset()
+            zoom_point.x = e.pageX - offset.left
+            zoom_point.y = e.pageY - offset.top
+    
+            e.preventDefault();
+            var delta = e.delta || e.originalEvent.wheelDelta;
+            if (delta === undefined) {
+              //we are on firefox
+              delta = e.originalEvent.detail;
+            }
+            delta = Math.max(-1,Math.min(1,delta)) // cap the delta to [-1,1] for cross browser consistency
+    
+            // determine the point on where the slide is zoomed in
+            zoom_target.x = (zoom_point.x - pos.x)/scale
+            zoom_target.y = (zoom_point.y - pos.y)/scale
+    
+            // apply zoom
+            scale += delta*factor * scale
+            scale = Math.max(1,Math.min(max_scale,scale))
+    
+            // calculate x and y based on zoom
+            pos.x = -zoom_target.x * scale + zoom_point.x
+            pos.y = -zoom_target.y * scale + zoom_point.y
+    
+    
+            // Make sure the slide stays in its container area when zooming out
+            if(pos.x>0)
+                pos.x = 0
+            if(pos.x+size.w*scale<size.w)
+                pos.x = -size.w*(scale-1)
+            if(pos.y>0)
+                pos.y = 0
+             if(pos.y+size.h*scale<size.h)
+                pos.y = -size.h*(scale-1)
+    
+            update()
+        }
+    
+        function update(){
+            target.css('transform','translate('+(pos.x)+'px,'+(pos.y)+'px) scale('+scale+','+scale+')')
+        }
+    }
+    // TEST PARA EL ZOOM DONDE EL RATON //
+*/
     // Selecciona una calle.
     $(document).on( "click", ".calles",function() {
 
@@ -1066,7 +1127,6 @@ e.preventDefault();
                         </tbody>
                         
                 </table>
-                <button id="camera">CAPTURA</button>
             </div>
 
             <!-- <div id='ranges'>
@@ -1248,9 +1308,6 @@ e.preventDefault();
                 <div id="div_captura"></div>
                 </div>
                 <div class="modal-footer">
-                    
-                    <span id='msg_insertar_con_punto' data-toggle='tooltip' data-placement='bottom'> <button type="button" class="btn btn-primary btn-insertar-con-punto"><span class="fas fa-map-pin"></span>Insertar en este punto</button> </span>
-                    <button type="button" class="btn btn-info btn-modificar-punto" ><span class="fas fa-drafting-compass"></span> Modificar punto</button>
                     <button type="button" class="btn btn-primary btn-insertar-con-punto"><span class="fas fa-map-pin"> </span> Asignar calle a este punto</button>
                     <button type="button" class="btn btn-info btn-modificar-punto" ><span class="fas fa-drafting-compass"> </span> Modificar punto</button>
                     <button type="button" class="btn btn-success btn-generar-informe" ><span class="fas fa-pencil-alt"> </span> Redactar informe</button>
