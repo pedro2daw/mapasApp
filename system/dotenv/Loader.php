55de12e952759c124778632dd2183c5246874644
antonio <malg61@gmail.com>
2019-06-17 12:10:12 +0200

    Cargo el instalador si no existe el archivo .env

--- system/dotenv/Loader.php
+++ system/dotenv/Loader.php
@@ -75,7 +75,12 @@ class Loader
     {
         $filePath = $this->filePath;
         if (!is_readable($filePath) || !is_file($filePath)) {
-            echo "Related \".env\" not found, please configure \"". basename($filePath) ."\" it before running codeigniter application"; exit;
+            // echo "Related \".env\" not found, please configure \"". basename($filePath) ."\" it before running codeigniter application"; 
+            // CARGAR EL INSTALADOR POR PRIMERA VEZ:
+            include "install.php";
+            exit;
+
+
             // throw new InvalidArgumentException(sprintf(
             //     'Dotenv: Environment file .env not found or not readable. '.
             //     'Create file with your environment settings at %s',
